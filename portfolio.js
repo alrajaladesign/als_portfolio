7da9b45e2d46f46541d0c842e0253af9ad46fa0d
alrajaladesign <al.rajala.design@gmail.com>
2026-02-12 12:07:45 -0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+
+const resume_button = document.getElementById('resume');
+
+resume_button.addEventListener('click', function(){
+
+    console.log('Resume button clicked');
+})
+
+
+})
